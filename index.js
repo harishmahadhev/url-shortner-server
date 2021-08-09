3a58f5239f20da355bcd876a505749a76fa42b73
harishmahadhev <harishmahadhev@gmail.com>
2021-08-09 11:52:00 +0530

    Port Error Fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express, { json } from 'express';
 import './database/db_connection.js'
 import { longurlRouter, shorturlRouter } from './routes/url_route.js';
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(json());
 app.use(express.urlencoded({ extended: false }))
 app.use("/shorturl", longurlRouter);
